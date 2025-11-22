b7ea81278966251dc80db926c6c1df1dbc0a4d9b
Diego Melendez <melramdiego77@gmail.com>
2025-11-22 13:44:55 -0600

    nav and colour of my layout is changed

--- templates/layout/default.php
+++ templates/layout/default.php
@@ -19,14 +19,12 @@
         <div class="nav_links" id="navLinks">
             <i class="fa fa-times" onclick="hideMenu()"></i>
             <ul>
-                <li><a href="/">HOME</a></li>
-                <li><a href="/trips/index">Trips</a></li>
-                <li><a href="/promotions/index">TRUCKS</a></li>
-                <li><a href="//index">BOXES</a></li>
-                <li><a href="/freights/index">TRIPS</a></li>
-                <li><a href="/clients/index">CLIENTS</a></li>
+                <li><a href="/">Home</a></li>
+                <li><a href="/vehicles/index">History</a></li>
+                <li><a href="/promotions/index">Promotions</a></li>
+                <li><a href="/users/view">My Profile</a></li>
 
-                <li><?= $this->Html->link('LOGOUT', ['controller' => 'Users', 'action' => 'logout']) ?></li>
+                <li><?= $this->Html->link('Log out', ['controller' => 'Users', 'action' => 'logout']) ?></li>
             </ul>
         </div>
         <i class="fa fa-bars" onclick="showMenu()"></i>
@@ -43,22 +41,21 @@
 
     <div class="footer-brand">
       <img src="/img/logo.png" alt="Our logo" class="footer-logo">
-      <p>&copy; 2025 Transport</p>
+      <p>&copy; 2025 Electromov</p>
     </div>
 
     <div class="footer-links">
       <h4>Quick Links</h4>
       <ul>
         <li><a href="#home">Home</a></li>
-        <li><a href="#drivers">Drivers</a></li>
-        <li><a href="#trucks">trucks</a></li>
+        <li><a href="#promotions">Promotions</a></li>
         <li><a href="#contact">Contact us</a></li>
       </ul>
     </div>
 
     <div class="footer-contact">
       <h4>Contact</h4>
-      <p>Email: transport@gmail.com</p>
+      <p>Email: electromov@gmail.com</p>
       <p>WhatsApp: +52 462 123 4567</p>
     </div>
 
